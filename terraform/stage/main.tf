8dafa55b6607401758a7a3a5965ac6e444f45596
Elijah Zobenko <i.zobenko@corp.mail.ru>
2022-04-19 01:11:21 +0300

    Update stage deployment

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -6,17 +6,22 @@ provider "yandex" {
 }
 
 module "app" {
-  source          = "../modules/app"
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
-  env             = "stage"
+  source           = "../modules/app"
+  env              = "stage"
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
+  app_disk_image   = var.app_disk_image
+  subnet_id        = var.subnet_id
+  enable_deploy    = true
+  database_ip      = module.db.external_ip_address_db
 }
 
 module "db" {
-  source          = "../modules/db"
-  public_key_path = var.public_key_path
-  db_disk_image   = var.db_disk_image
-  subnet_id       = var.subnet_id
-  env             = "stage"
+  source           = "../modules/db"
+  env              = "stage"
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
+  db_disk_image    = var.db_disk_image
+  subnet_id        = var.subnet_id
+  enable_deploy    = true
 }
